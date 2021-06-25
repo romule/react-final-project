3a412ced853010030ddce22abf9e82bb42ddb9d1
Vladyslav Katerusha <vladyslav.katerusha@forforce.com>
2021-06-25 21:36:06 +0300

    fix rep

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import './App.scss';
 import React from 'react';
+import Header from '../Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello final project</h1>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <h1>Hello final project</h1>
+      </div>
+    </>
   );
 }
 
