7eef956593f969d57fde4aec1e22f962ec040df9
Elijah Romashchenko <romashbis@gmail.com>
2021-06-29 20:20:09 +0300

    start from begining & add axios

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,6 +2,19 @@ import './App.scss';
 import React from 'react';
 import Header from '../Header';
 
+const axios = require('axios').defualt;
+
+axios
+  .get('https://abcshopuk.herokuapp.com/api/products')
+  .then((products) => {
+    // custom consle
+    console.log(products);
+  })
+  .catch((err) => {
+    // custom consle
+    console.warn(err);
+  });
+
 function App() {
   return (
     <>
