2a93d9b1b8d255286503f53b3fec20c9a3fdc411
Elijah Romashchenko <romashbis@gmail.com>
2021-06-29 19:23:48 +0300

    adding new DB

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://RiO:kX5YpagueslpyoUk@abcphotodb.jzayn.mongodb.net/test?authSource=admin&replicaSet=atlas-j1u28v-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
-  secretOrKey: "random very very secret string",
+    'mongodb+srv://elijah:jie4aaEbtZu5AXZI@abcgoods.mgeyc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: '$tring',
 };
