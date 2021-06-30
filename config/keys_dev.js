a10ef9455ccc3561787a247a78d8696d236fd9e1
Elijah Romashchenko <romashbis@gmail.com>
2021-06-30 16:15:13 +0300

    ceep tying

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb+srv://elijah:jie4aaEbtZu5AXZI@abcgoods.mgeyc.mongodb.net/abcphotoDB?retryWrites=true&w=majority',
+    'mongodb+srv://elijah:jie4aaEbtZu5AXZI@abcgoods.mgeyc.mongodb.net/abcphotodb?retryWrites=true&w=majority',
   secretOrKey: '$tring',
 };
