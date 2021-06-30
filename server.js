e9a5e3cc7165f8e0c3d4cf0e8fa3228522b0b86d
Elijah Romashchenko <romashbis@gmail.com>
2021-06-30 17:54:18 +0300

    back-up the seting for server.js

--- server.js
+++ server.js
@@ -70,10 +70,10 @@ app.use("/", mainRoute);
 // Server static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
